aa615670e8cc62d5f5735ea066535e062a8a4af0
Alzando <123951867+allzndd@users.noreply.github.com>
2025-12-05 13:40:25 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,7 +41,9 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime'
+        'email_verified_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
 
     /**
