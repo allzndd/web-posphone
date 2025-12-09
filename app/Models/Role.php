c71a4af4da0515b51d73e3349a9940a458c93b08
Alzando <123951867+allzndd@users.noreply.github.com>
2025-12-09 18:51:48 +0700

    Create Role.php

---
+++ app/Models/Role.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'role';
+
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama',
+        'slug',
+    ];
+
+    /**
+     * Get the users for the role.
+     */
+    public function users()
+    {
+        return $this->hasMany(User::class, 'role_id');
+    }
+}
