064852068d170303f973d3f7a3631920ddd762f2
Alzando <123951867+allzndd@users.noreply.github.com>
2025-12-16 03:38:46 +0700

    Update PosProdukMerk.php

--- app/Models/PosProdukMerk.php
+++ app/Models/PosProdukMerk.php
@@ -12,9 +12,6 @@ class PosProdukMerk extends Model
 
     protected $table = 'pos_produk_merk';
 
-    // Disable timestamps since the table doesn't have created_at/updated_at columns
-    public $timestamps = false;
-
     protected $fillable = [
         'owner_id',
         'nama',
