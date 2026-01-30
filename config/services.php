1a17abc474e60646ca1dd6820e737be07bdb806f
Alzando <123951867+allzndd@users.noreply.github.com>
2026-01-31 00:44:59 +0700

    Update services.php

--- config/services.php
+++ config/services.php
@@ -31,4 +31,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'groq' => [
+        'api_key' => env('GROQ_API_KEY'),
+        'model' => env('GROQ_MODEL', 'llama3-8b-8192'),
+    ],
+
 ];
