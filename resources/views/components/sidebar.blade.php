1583a1ee3af8e691095cb2765f8a58578486e4c9
Alzando <123951867+allzndd@users.noreply.github.com>
2026-02-04 03:42:46 +0700

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -197,15 +197,6 @@
                         </a>
                     </li>
                     <li class="relative mb-2 flex hover:cursor-pointer">
-                        <a href="{{ route('produk.create') }}" class="w-full">
-                            <div class="my-[3px] flex cursor-pointer items-center py-2 pl-[60px] pr-8">
-                                <p class="leading-1 flex text-sm {{ Request::is('produk/create') ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600 dark:text-gray-400' }}">
-                                    New Product
-                                </p>
-                            </div>
-                        </a>
-                    </li>
-                    <li class="relative mb-2 flex hover:cursor-pointer">
                         <a href="{{ route('pos-produk-merk.index') }}" class="w-full">
                             <div class="my-[3px] flex cursor-pointer items-center py-2 pl-[60px] pr-8">
                                 <p class="leading-1 flex text-sm {{ Request::is('pos-produk-merk*') ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600 dark:text-gray-400' }}">
