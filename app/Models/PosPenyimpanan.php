211759c74171b7d1f8ae2206bc116f829376f3cf
Alzando <123951867+allzndd@users.noreply.github.com>
2026-02-04 03:36:24 +0700

    Create PosPenyimpanan.php

---
+++ app/Models/PosPenyimpanan.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PosPenyimpanan extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pos_penyimpanan';
+
+    protected $fillable = [
+        'id_owner',
+        'kapasitas',
+        'id_global',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Relationship: Penyimpanan belongs to Owner
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'id_owner');
+    }
+}
