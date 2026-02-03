2060c2db798a0f8328980443900f250dd1e3b3a7
Alzando <123951867+allzndd@users.noreply.github.com>
2026-02-04 05:23:34 +0700

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -531,23 +531,6 @@
         @endif
 
         @if(auth()->user()->isSuperadmin())
-        <!-- Services -->
-        <li class="relative mb-3 flex hover:cursor-pointer">
-            <a href="{{ route('layanan.index') }}" class="w-full">
-                <div class="my-[3px] flex cursor-pointer items-center px-8">
-                    <span class="{{ Request::is('layanan*') ? 'font-bold text-brand-500 dark:text-white' : 'font-medium text-gray-600' }}">
-                        <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"></path></svg>
-                    </span>
-                    <p class="leading-1 ml-4 flex {{ Request::is('layanan*') ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600' }}">
-                        Services
-                    </p>
-                </div>
-                @if(Request::is('layanan*'))
-                    <div class="absolute right-0 top-px h-9 w-1 rounded-lg bg-brand-500 dark:bg-brand-400"></div>
-                @endif
-            </a>
-        </li>
-
         <!-- Service Packages -->
         <li class="relative mb-3 flex hover:cursor-pointer">
             <a href="{{ route('paket-layanan.index') }}" class="w-full">
