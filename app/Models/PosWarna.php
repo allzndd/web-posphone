8f4075e4031d962f4bd3a8dda356332d7cae9e52
Alzando <123951867+allzndd@users.noreply.github.com>
2026-02-04 03:36:27 +0700

    Create PosWarna.php

---
+++ app/Models/PosWarna.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PosWarna extends Model
+{
+    use HasFactory;
+
+    protected $table = 'pos_warna';
+
+    protected $fillable = [
+        'id_owner',
+        'warna',
+        'is_global',
+    ];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Relationship: Warna belongs to Owner
+     */
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'id_owner');
+    }
+}
