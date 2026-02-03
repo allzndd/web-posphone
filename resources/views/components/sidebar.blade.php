7906377296f9321d6209eebccbb7fcb59ac91459
Alzando <123951867+allzndd@users.noreply.github.com>
2026-02-04 06:26:16 +0700

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -568,14 +568,14 @@
 
         @if(auth()->user()->isSuperadmin())
         <!-- Data Master Dropdown -->
-        <li class="relative mb-3" x-data="{ open: {{ Request::is('pos-penyimpanan*') || Request::is('pos-warna*') || Request::is('pos-ram*') ? 'true' : 'false' }} }">
+        <li class="relative mb-3" x-data="{ open: {{ Request::is('pos-penyimpanan*') || Request::is('pos-warna*') || Request::is('pos-ram*') || Request::is('pos-produk-merk*') ? 'true' : 'false' }} }">
             <div class="flex hover:cursor-pointer" @click="open = !open">
                 <div class="w-full">
                     <div class="my-[3px] flex cursor-pointer items-center px-8">
-                        <span class="{{ Request::is('pos-penyimpanan*') || Request::is('pos-warna*') || Request::is('pos-ram*') ? 'font-bold text-brand-500 dark:text-white' : 'font-medium text-gray-600 dark:text-gray-400' }}">
+                        <span class="{{ Request::is('pos-penyimpanan*') || Request::is('pos-warna*') || Request::is('pos-ram*') || Request::is('pos-produk-merk*') ? 'font-bold text-brand-500 dark:text-white' : 'font-medium text-gray-600 dark:text-gray-400' }}">
                             <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"></path><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"></path></svg>
                         </span>
-                        <p class="leading-1 ml-4 flex {{ Request::is('pos-penyimpanan*') || Request::is('pos-warna*') || Request::is('pos-ram*') ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600 dark:text-gray-400' }}">
+                        <p class="leading-1 ml-4 flex {{ Request::is('pos-penyimpanan*') || Request::is('pos-warna*') || Request::is('pos-ram*') || Request::is('pos-produk-merk*') ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600 dark:text-gray-400' }}">
                             Data Master
                         </p>
                         <span class="ml-auto mr-3 text-gray-600 transition-transform duration-200" :class="{ 'rotate-180': open }">
@@ -584,11 +584,11 @@
                     </div>
                 </div>
             </div>
-            @if(Request::is('pos-penyimpanan*') || Request::is('pos-warna*') || Request::is('pos-ram*'))
+            @if(Request::is('pos-penyimpanan*') || Request::is('pos-warna*') || Request::is('pos-ram*') || Request::is('pos-produk-merk*'))
                 <div class="absolute right-0 top-px h-9 w-1 rounded-lg bg-brand-500 dark:bg-brand-400"></div>
             @endif
             <!-- Submenu -->
-            <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="opacity-0 transform scale-y-95" x-transition:enter-end="opacity-100 transform scale-y-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="opacity-100 transform scale-y-100" x-transition:leave-end="opacity-0 transform scale-y-95" class="overflow-hidden" @if(!Request::is('pos-penyimpanan*') && !Request::is('pos-warna*') && !Request::is('pos-ram*')) style="display: none;" @endif>
+            <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="opacity-0 transform scale-y-95" x-transition:enter-end="opacity-100 transform scale-y-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="opacity-100 transform scale-y-100" x-transition:leave-end="opacity-0 transform scale-y-95" class="overflow-hidden" @if(!Request::is('pos-penyimpanan*') && !Request::is('pos-warna*') && !Request::is('pos-ram*') && !Request::is('pos-produk-merk*')) style="display: none;" @endif>
                 <ul class="my-[3px]">
                     <li class="relative mb-2 flex hover:cursor-pointer">
                         <a href="{{ route('pos-penyimpanan.index') }}" class="w-full">
@@ -613,6 +613,15 @@
                             <div class="my-[3px] flex cursor-pointer items-center py-2 pl-[60px] pr-8">
                                 <p class="leading-1 flex text-sm {{ Request::is('pos-ram*') ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600 dark:text-gray-400' }}">
                                     RAM
+                                </p>
+                            </div>
+                        </a>
+                    </li>
+                    <li class="relative mb-2 flex hover:cursor-pointer">
+                        <a href="{{ route('pos-produk-merk.index') }}" class="w-full">
+                            <div class="my-[3px] flex cursor-pointer items-center py-2 pl-[60px] pr-8">
+                                <p class="leading-1 flex text-sm {{ Request::is('pos-produk-merk*') ? 'font-bold text-navy-700 dark:text-white' : 'font-medium text-gray-600 dark:text-gray-400' }}">
+                                    Product Names
                                 </p>
                             </div>
                         </a>
