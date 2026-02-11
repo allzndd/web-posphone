d9ef1c19726b64f5448c38a5a914e9000931e6bc
Alzando <123951867+allzndd@users.noreply.github.com>
2026-02-12 04:58:16 +0700

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/email/verify';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
