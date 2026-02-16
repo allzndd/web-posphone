3b9c783336ae0aa1926aa01349eb73331d3179f9
Alzando <123951867+allzndd@users.noreply.github.com>
2026-02-16 14:12:17 +0700

    Update finance.blade.php

--- resources/views/pages/settings/partials/finance.blade.php
+++ resources/views/pages/settings/partials/finance.blade.php
@@ -104,19 +104,4 @@
             <p class="mt-2 text-sm text-red-500 dark:text-red-400">{{ $message }}</p>
         @enderror
     </div>
-
-    <!-- Info Box -->
-    <div class="rounded-xl bg-blue-50 dark:bg-blue-900/20 border border-blue-200 dark:border-blue-800/50 p-4">
-        <div class="flex items-start gap-3">
-            <svg class="w-5 h-5 text-blue-600 dark:text-blue-400 mt-0.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20">
-                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"/>
-            </svg>
-            <div class="flex-1">
-                <p class="text-sm font-medium text-blue-800 dark:text-blue-200">Currency Format</p>
-                <p class="mt-1 text-xs text-blue-700 dark:text-blue-300">
-                    All prices throughout the application will be displayed in your selected currency format with appropriate thousand separators.
-                </p>
-            </div>
-        </div>
-    </div>
 </div>
